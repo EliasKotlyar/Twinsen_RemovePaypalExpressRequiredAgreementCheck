f54ff0fef4a0e90c3adf3603e85a9dd34db4c965
Eko <elias.kotlyar@gmail.com>
2015-05-29 23:59:52 +0200

    Functional Version

--- app/code/local/Twinsen/RemovePaypalExpressRequiredAgreementCheck/Helper/Data.php
+++ app/code/local/Twinsen/RemovePaypalExpressRequiredAgreementCheck/Helper/Data.php
@@ -1,29 +1,21 @@
 <?php
 
 /**
- * hotbytes - the opensource company
- *
- *
- * @category Extbase
- * @package ${Package}
- * @author Elias Kotlyar <eko@hotbytes.de
+ * @category Magento
+ * @package Twinsen_RemovePaypalExpressRequiredAgreementCheck
+ * @author Elias Kotlyar <elias.kotlyar@gmail.com>
  * @date 29.05.2015
- * @copyright Copyright (c) 2014 hotbytes GmbH & Co KG (http://www.hotbytes.de/)
+ * @copyright Copyright (c) 2015 Elias Kotlyar
  * @license http://opensource.org/licenses/osl-3.0.php Open Software License (OSL 3.0)
  */
 class Twinsen_RemovePaypalExpressRequiredAgreementCheck_Helper_Data extends Mage_Checkout_Helper_Data
 {
     public function getRequiredAgreementIds()
     {
-        $request = Mage::app()->getRequest():
-         // get Controller name
-        $request->getControllerName();
-        // get Action name, i.e. the function inside the controller
-        $request->getActionName();
-        // get Router name
-        $request->getRouteName();
-        // get module name
-        $request->getModuleName();
+        $request = Mage::app()->getRequest();
+        if ($request->getControllerName() == 'express' && $request->getActionName() == 'placeOrder' && $request->getRouteName() == 'paypal' && $request->getModuleName() == 'paypal') {
+            return null;
+        }
         return parent::getRequiredAgreementIds();
     }
 }
